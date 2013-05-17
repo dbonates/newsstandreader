2b8a3611b8a1600aae0facfdd8703ef3c8bc9b9c
Daniel Bonates <danielbonates@dbombp.local>
2013-05-17 09:43:12 -0300

    Bugado-fiquei bolado
    as cells nao aparecem

---
+++ Reader/Wrapper.h
@@ -0,0 +1,13 @@
+//
+//  Wrapper.h
+//  Reader
+//
+//  Created by Daniel Bonates on 5/14/13.
+//  Copyright (c) 2013 Bonates. All rights reserved.
+//
+
+@interface Wrapper : NSObject
+
++ (Wrapper *)sharedInstance;
+
+@end
